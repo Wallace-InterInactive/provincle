446ac58e9dd994ef52e3a3a00d1956b04c64ceca
ppesti <oteka2002@gmail.com>
2024-03-10 19:23:00 +0100

    configure project tools

---
+++ docs/dev.md
@@ -0,0 +1,7 @@
+# Development Guide
+
+**Installing dependencies**: `yarn`
+
+**Running ESLint to lint the code**: `yarn lint`
+
+**Running Prettier to format the code**: `yarn format`
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <span className="uppercase font-bold">
+      prov<span className="text-green-600">i</span>ncle
+    </span>
+  );
+};
+
+export default App;
