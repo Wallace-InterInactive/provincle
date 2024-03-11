768e49f5edbaa3332e688d9eb736618471161095
ppesti <oteka2002@gmail.com>
2024-03-11 19:20:38 +0100

    format code
    Break lines that are over 80 characters.

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,23 @@ import GuessSubmit from "./components/body/GuessSubmit";
 
 const App: React.FC = () => {
   return (
-    <div className="flex flex-col justify-between items-center min-h-screen dark:bg-slate-900 dark:text-slate-50">
+    <div
+      className={
+        "flex flex-col justify-between " +
+        "items-center min-h-screen dark:bg-slate-900 dark:text-slate-50"
+      }
+    >
       <div>
         <div className="w-full max-w-lg flex flex-col">
           <header className="border-b-2 border-gray-200 flex mb-4">
             <Help />
 
-            <h1 className="text-4xl font-bold uppercase tracking-wide text-center my-1 flex-auto">
+            <h1
+              className={
+                "text-4xl font-bold uppercase" +
+                "tracking-wide text-center my-1 flex-auto"
+              }
+            >
               🇨🇦 <Provincle />
             </h1>
 
