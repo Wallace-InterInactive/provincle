8deb74c6087082f2c25c803cd20f73a436f6d549
ppesti <oteka2002@gmail.com>
2024-03-13 13:56:24 +0100

    update docs, index.html, and cleanup code

--- src/services/Utils.ts
+++ src/services/Utils.ts
@@ -1,58 +1,7 @@
-// TODO: refactor function
+import accentsMap from "./AccentsMap";
+
 export function sanitizeString(str: string): string {
   let retVal = str.trim().toLowerCase();
-
-  const accentsMap: { [accented: string]: string } = {
-    á: "a",
-    é: "e",
-    í: "i",
-    ó: "o",
-    ú: "u",
-    à: "a",
-    è: "e",
-    ì: "i",
-    ò: "o",
-    ù: "u",
-    â: "a",
-    ê: "e",
-    î: "i",
-    ô: "o",
-    û: "u",
-    ä: "a",
-    ë: "e",
-    ï: "i",
-    ö: "o",
-    ü: "u",
-    ã: "a",
-    õ: "o",
-    ñ: "n",
-    ç: "c",
-    Á: "A",
-    É: "E",
-    Í: "I",
-    Ó: "O",
-    Ú: "U",
-    À: "A",
-    È: "E",
-    Ì: "I",
-    Ò: "O",
-    Ù: "U",
-    Â: "A",
-    Ê: "E",
-    Î: "I",
-    Ô: "O",
-    Û: "U",
-    Ä: "A",
-    Ë: "E",
-    Ï: "I",
-    Ö: "O",
-    Ü: "U",
-    Ã: "A",
-    Õ: "O",
-    Ñ: "N",
-    Ç: "C",
-    ß: "ss",
-  };
 
   for (const accentedChar in accentsMap) {
     const regex = new RegExp(accentedChar, "g");
