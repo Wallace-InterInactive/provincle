8842b5c30b2814e0b993f4eb1aaa3b2fd40be133
ppesti <oteka2002@gmail.com>
2024-08-09 14:53:45 +0200

    remove unnecessary right margin

--- src/components/GuessButton/GuessButton.tsx
+++ src/components/GuessButton/GuessButton.tsx
@@ -8,7 +8,7 @@ export function GuessButton({ onClick, text }: GuessButtonProps) {
     <button
       type="submit"
       onClick={onClick}
-      className="dark:bg-slate-800 border-2 rounded uppercase flex-shrink-0 p-1 mr-1 font-semibold"
+      className="dark:bg-slate-800 border-2 rounded uppercase flex-shrink-0 p-1 font-semibold"
     >
       {text}
     </button>
