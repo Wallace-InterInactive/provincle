e4623f6f1c8b9964fcc0ef1547df43e58632901d
Péter Pesti <90276959+ppesti@users.noreply.github.com>
2024-08-09 20:43:20 +0200

    extract input and button (#15)
    * extract input and button
    
    Create AutoSuggestInput and GuessButton components with minor style patches.
    
    * remove unnecessary right margin

---
+++ src/components/AutoSuggestInput/AutoSuggestInput.tsx
@@ -0,0 +1,55 @@
+import Autosuggest from "react-autosuggest";
+import { useState } from "react";
+import { fetchSuggestions } from "../../utils/utils.ts";
+
+interface AutoSuggestInputProps {
+  currentGuess: string;
+  setCurrentGuess: React.Dispatch<React.SetStateAction<string>>;
+  placeholder: string;
+  suggestionsArray: string[];
+}
+
+export function AutoSuggestInput({
+  currentGuess,
+  setCurrentGuess,
+  placeholder,
+  suggestionsArray,
+}: AutoSuggestInputProps) {
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  return (
+    <>
+      <Autosuggest
+        id="map-autosuggest"
+        suggestions={suggestions}
+        getSuggestionValue={suggestion => suggestion}
+        inputProps={{
+          value: currentGuess,
+          placeholder: placeholder,
+          onChange: (_e, { newValue }) => setCurrentGuess(newValue),
+          className: "w-full dark:bg-slate-800 dark:text-slate-100",
+        }}
+        onSuggestionsFetchRequested={({ value }) =>
+          setSuggestions(fetchSuggestions(suggestionsArray, value))
+        }
+        onSuggestionsClearRequested={() => setSuggestions([])}
+        renderSuggestion={suggestion => (
+          <div className="dark:bg-slate-800 border-2 rounded dark:text-slate-100">
+            {suggestion}
+          </div>
+        )}
+        renderSuggestionsContainer={({ containerProps, children }) => (
+          <div
+            {...containerProps}
+            className={`${containerProps.className} absolute bottom-full w-full bg-white mb-1 divide-x-2 max-h-52 overflow-auto`}
+          >
+            {children}
+          </div>
+        )}
+        containerProps={{
+          className:
+            "dark:bg-slate-800 border-2 rounded flex-auto relative p-1 mr-1",
+        }}
+      />
+    </>
+  );
+}
---
+++ src/components/GuessButton/GuessButton.tsx
@@ -0,0 +1,16 @@
+interface GuessButtonProps {
+  onClick: () => void;
+  text: string;
+}
+
+export function GuessButton({ onClick, text }: GuessButtonProps) {
+  return (
+    <button
+      type="submit"
+      onClick={onClick}
+      className="dark:bg-slate-800 border-2 rounded uppercase flex-shrink-0 p-1 font-semibold"
+    >
+      {text}
+    </button>
+  );
+}
