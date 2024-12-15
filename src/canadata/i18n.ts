cbc3f0b1d8d267aa9b219b99906b446b03eff3f8
szalabala <szalabala@yahoo.co.uk>
2024-12-15 23:18:57 +0100

    added extra to Finale page

--- src/canadata/i18n.ts
+++ src/canadata/i18n.ts
@@ -31,6 +31,7 @@ i18next
           gameNeighborRoundInstruction: "What are the neighbours",
           gameMajorLeagueRoundInstruction: "Which team is from",
           gamePotRoundFinaleStats: "Stats",
+          gameTryOurOtherGames: "Please test your knowledge in the following games",
           // alerts
           unknownPot: "Unknown province ot territory!",
           unknownCity: "Unknown city!",
