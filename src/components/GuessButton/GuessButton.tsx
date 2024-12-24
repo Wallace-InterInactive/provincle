29ad108b67b6838508a2a4dff37a6b55a96d1bcf
Peter Pesti <90276959+ppesti@users.noreply.github.com>
2024-12-24 11:54:37 +0100

    add component tests (#53)
    * add component tests
    
    - for
      - Settings
      - GuessButton
    - exclude test files from coverage report
    
    * add tests for `NextRoundButton`
    
    * add tests for `NextRoundButton`

--- src/components/GuessButton/GuessButton.tsx
+++ src/components/GuessButton/GuessButton.tsx
@@ -1,11 +1,12 @@
 import { GuessButtonProps } from "../../types/GuessButtonProps.ts";
 
-export function GuessButton({ onClick, text }: GuessButtonProps) {
+export function GuessButton({ handler, text }: GuessButtonProps) {
   return (
     <button
       type="submit"
-      onClick={onClick}
+      onClick={handler}
       className="dark:bg-slate-800 dark:hover:bg-slate-700 border-2 rounded uppercase flex-shrink-0 p-1 font-semibold"
+      data-testid="guess-btn"
     >
       {text}
     </button>
